7c5e180622b665128dd05fd2d52e371f5b97e476
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2025-09-08 20:12:58 -0300

    Feat: Adiciona LittleFS como Sistema de Arquivos
    Antes de servirmos qualquer arquivo, devemos ser capazes de acessar tais
    arquivos. LittleFS irá ser o nosso Sistema de Arquivos que irá nos
    permitir ler, escrever, modificar e deletar arquivos no nosso sistema.

--- server/server.ino
+++ server/server.ino
@@ -1,3 +1,4 @@
+#include <LittleFS.h>
 #include <WiFi.h>
 
 // Credenciais de acesso à rede.
@@ -7,6 +8,12 @@ const char *password = "12345678";
 void setup() {
   // Inicializa comunicação serial.
   Serial.begin(115200);
+
+  // Inicializa do Sistema de Arquivos Interno.
+  if (!LittleFS.begin()) {
+    Serial.println("Um erro ocorreu ao montar LittleFS");
+    return;
+  }
 
   // Cria ponto de acesso wireless.
   WiFi.softAP(ssid, password);
