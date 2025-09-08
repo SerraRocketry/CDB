f5e7e420aa907ef0e6dabbd489ef393543cc5d35
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2025-09-08 19:49:57 -0300

    Feat: Adiciona Acess Point LAN ao ESP32
    Não adianta hospedar um servidor se não formos capazes de conectar a
    ele. A lib Wifi.h permite com que o ESP32 crie ou acesse uma rede LAN,
    nos dando assim tal acesso.

--- server/server.ino
+++ server/server.ino
@@ -1,6 +1,17 @@
+#include <WiFi.h>
+
+// Credenciais de acesso à rede.
+const char *ssid = "Servidor ESP32";
+const char *password = "12345678";
+
 void setup() {
   // Inicializa comunicação serial.
   Serial.begin(115200);
+
+  // Cria ponto de acesso wireless.
+  WiFi.softAP(ssid, password);
+  Serial.println("Criando ponto de acesso WiFi..");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
