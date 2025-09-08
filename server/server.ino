1bf3fe85770c97ce077b8403d40e4a987c45aa48
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2025-09-08 20:27:35 -0300

    Feat: Adiciona servidor HTTP ao ESP32
    Além de inicializar o servidor também criei rotas básicas para os
    arquivos do site que enviei para o LittleFS.

--- server/server.ino
+++ server/server.ino
@@ -1,9 +1,24 @@
+#include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 #include <WiFi.h>
 
 // Credenciais de acesso à rede.
 const char *ssid = "Servidor ESP32";
 const char *password = "12345678";
+
+// Instancia um servidor http que escutará na porta 80.
+AsyncWebServer server(80);
+
+void setServerRoutes() {
+  // Rotas básicas de acesso ao site para o usuário
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(LittleFS, "/style.css", "text/css");
+  });
+}
 
 void setup() {
   // Inicializa comunicação serial.
@@ -19,6 +34,10 @@ void setup() {
   WiFi.softAP(ssid, password);
   Serial.println("Criando ponto de acesso WiFi..");
   Serial.println(WiFi.softAPIP());
+
+  // Configura as rotas do servidor e o inicializa.
+  setServerRoutes();
+  server.begin();
 }
 
 void loop() {
